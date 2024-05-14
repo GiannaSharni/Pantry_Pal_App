9305491dc28b082ddfb4368ddbf0a0a7114a88bb
Rumaysaa Mohamed <rumaysaa15@gmail.com>
2024-05-14 15:46:44 +0100

    recipe controller

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,11 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :favorites
   has_many :reviews
+
+  include PgSearch::Model
+  pg_search_scope :search_by_title,
+    against: [ :title ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
