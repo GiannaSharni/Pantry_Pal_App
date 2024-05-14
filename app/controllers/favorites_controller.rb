7f7abecd4c4b2cc3467e1fd4e66ff5a04438c509
Gianna Sharni <GiannaSharni@gmail.com>
2024-05-14 15:28:53 +0100

    fav 1st

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,12 @@
 class FavoritesController < ApplicationController
+  def index
+    @favorites = Favorite.all
+  end
+
+  def add_favorite
+    @recipe = Recipe.find(params[:recipe_id])
+    current_user.favorite(@recipe)
+    @favorite.save
+    redirect_to recipe_path(@recipe)
+  end
 end
