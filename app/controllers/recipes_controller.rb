c060f85baaa4d094ec515bf546b2a3e058041974
Khalil Hersi <khalilhersi2000@gmail.com>
2024-05-17 14:05:18 +0100

    API Fixed

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,14 +1,10 @@
 class RecipesController < ApplicationController
   def index
     @ingredients = Recipe.search
-    p ''
-    p @ingredients
-    p params['recipe']['ingredients']
-    p ''
     @recent_recipes = ['Lasagna', 'White Chocolate Big Cake']
     # access info in api and add these key words together
     @recipe = Recipe.new
-    if params['recipe']['ingredients'].present?
+    if params['recipe'].present? && params['recipe']['ingredients'].present?
       @recipes = Recipe.search(params['recipe']['ingredients'])
       # redirect_to recipes_path(@recipes) fix this one with new page
     else
