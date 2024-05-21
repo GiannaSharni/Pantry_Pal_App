6e1ed735045f4db6f74e871decf0d37a5da0e39c
Rumaysaa Mohamed <rumaysaa15@gmail.com>
2024-05-21 16:33:23 +0100

    fixed recent recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,14 +1,14 @@
 class RecipesController < ApplicationController
   def index
     @ingredients = Recipe.search
-    @recent_recipes = ['Lasagna', 'White Chocolate Big Cake']
+    @recent_recipes = current_user.recipes.order(created_at: :desc).limit(3)
     # access info in api and add these key words together
     @recipe = Recipe.new
     if params['recipe'].present? && params['recipe']['ingredients'].present?
       @recipes = Recipe.search(params['recipe']['ingredients'])
       # redirect_to recipes_path(@recipes) fix this one with new page
     else
-      @recipes = Recipe.all
+      @recipes = []
       flash[:alert] = "No results found"
     end
   end
@@ -16,6 +16,7 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.search_recipe(params[:id])
     @favorite = current_user.favorites.find_by_id(params[:id])
+    @recipe_review = Recipe.find_by_id(params[:id])
   end
 
   # private
