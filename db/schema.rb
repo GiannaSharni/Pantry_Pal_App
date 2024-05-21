015cc46827212f8be13d7b5aad6b71dda6f399c0
Sandra Adeyinka <sandraadeyinka@hotmail.com>
2024-05-21 16:39:34 +0100

    add stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_15_155635) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_21_135141) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_tables", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "favorites", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -27,6 +32,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_15_155635) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -76,6 +89,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_15_155635) do
 
   add_foreign_key "favorites", "recipes"
   add_foreign_key "favorites", "users"
+  add_foreign_key "messages", "users"
   add_foreign_key "recipes", "users"
   add_foreign_key "recipes_ingredients", "ingredients", column: "ingredients_id"
   add_foreign_key "recipes_ingredients", "recipes"
