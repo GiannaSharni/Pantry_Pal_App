331200f0ccc3429e9bfb6c131c13de097b30e75b
Rumaysaa Mohamed <145496719+Gladiolu@users.noreply.github.com>
2024-05-22 10:35:25 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,16 +6,13 @@ Rails.application.routes.draw do
   resources :recipes do
     resources :reviews, only: [:new, :create]
 
-    resources :posts
-    post '/posts/:id/submit_review', to: 'posts#submit_review', as: 'submit_review'
-
+    
     post "add_favorite", to: "recipes#add_favorite", as: :add_favorite
   end
 
   get 'favorites', to: 'favorites#index'
   post 'favorites/:recipe_id', to: 'favorites#add_favorite', as: 'add_favorite'
   delete 'favorites/:recipe_id', to: 'favorites#remove_favorite', as: 'remove_favorite'
-  post '/recipes/:id/submit_review', to: 'recipes#submit_review', as: 'submit_review'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
