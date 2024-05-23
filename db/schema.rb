4d3231b6e7ccb436cca7c4a5bbbd5e4afdd54817
Jake Pople <jakepople@hotmail.co.uk>
2024-05-23 11:07:34 +0100

    reviews are now being created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_23_083629) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_23_094730) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -76,10 +76,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_23_083629) do
   create_table "reviews", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "comment"
-    t.string "raiting"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "recipe_id"
+    t.integer "rating"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
